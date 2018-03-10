d7832fbc86fa6f618e4a1eb1eb5afa59fedd7b08
keith-ferney <keith@ferney.org>
2018-03-10 16:36:34 -0700

    script update

--- minecraftInstaller.sh
+++ minecraftInstaller.sh
@@ -1,14 +1,29 @@
-sudo mkdir /opt/minecraft/
-cd /opt/minecraft/
-sudo wget https://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar
-sudo wget https://s3.amazonaws.com/Minecraft.Download/versions/1.8/minecraft_server.1.8.jar
-sudo wget -O minecraft_server https://www.dropbox.com/s/h5pcqhjklt77id0/minecraft_server?dl=0
-sudo wget -O minecraft https://www.dropbox.com/s/figdaktu3vd67yu/minecraft?dl=0
-sudo wget -O minecraft.png https://www.dropbox.com/s/cq0hycm5v4z8vp4/minecraft.png?dl=0
-cd /usr/share/applications/
-sudo wget -O Minecraft.desktop https://www.dropbox.com/s/pacgt5ugcxpshug/Minecraft.desktop?dl=0
-cd /bin/
-sudo wget -O minecraft_server https://www.dropbox.com/s/h5pcqhjklt77id0/minecraft_server?dl=0
-sudo wget -O minecraft https://www.dropbox.com/s/figdaktu3vd67yu/minecraft?dl=0
-sudo chmod +x minecraft_server
-sudo chmod +x minecraft
+#!/bin/bash
+
+tmpfile=/tmp/minecrafttempfile.tmp
+downloadurl="https://minecraft.net/en-us/download/server"
+serverurl=`egrep -io 'https.*game\/(.*)\/server.jar' $tmpfile`
+loc=/opt/minecraft
+
+sudo mkdir $loc
+
+echo "Grabbing minecraft download page..."
+curl -s $downloadurl > $tmpfile
+
+echo "Downloading server jar..."
+sudo wget -q -O $loc/minecraft_server.jar $serverurl 
+
+echo "Downloading client jar..."
+sudo wget -q -O $loc/Minecraft.jar https://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar
+
+echo "Downloading Minecraft.png"
+sudo wget -q -O minecraft.png https://raw.githubusercontent.com/keith-ferney/MinecraftScript/master/minecraft.png
+
+echo "Downloading desktop file"
+sudo wget -q -O /usr/share/applications/Minecraft.desktop https://raw.githubusercontent.com/keith-ferney/MinecraftScript/master/Minecraft.desktop
+
+echo "Downloading executables"
+sudo wget -q -O /usr/bin/minecraft https://raw.githubusercontent.com/keith-ferney/MinecraftScript/master/minecraft
+sudo wget -q -O /usr/bin/minecraft_server https://raw.githubusercontent.com/keith-ferney/MinecraftScript/master/minecraft_server 
+sudo chmod +x /bin/minecraft_server
+sudo chmod +x /bin/minecraft
